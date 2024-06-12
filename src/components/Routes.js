8259b63b85813aae483f992b9d664fe38bee7bc2
Ying Ying <yingyingfengg@gmail.com>
2024-06-12 16:27:44 -0400

    working cooper hewitt api, trying to get info i need

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,19 +1,3 @@
-// // find objects with images 
-// var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
-// var queryString = $.param({
-//     apikey: "8e787d5e-154a-4abd-877c-06d4c150ee6a",
-//     hasimage: 1
-    
-// });
-
-// $.getJSON(apiEndpointBaseURL + "?" + queryString, function(data) {
-//     console.log(data); 
-//     var records = data.records
-//     var title = records.map(record => record.title)
-//     var content = "<p>" + title.join('<br />')
-//     $("#title").append(content)
-// });
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch, Link, Redirect } from 'react-router-dom'
@@ -33,8 +17,7 @@ class Routes extends Component {
         this.state = {
             harvard: [],
             chicago: [],
-            smith: [],
-            louvre: []
+            cooper: []
         }
     }
     componentDidMount() {
@@ -50,18 +33,30 @@ class Routes extends Component {
             this.setState({ chicago: data })
             // console.log(data, 'real chicago')
         })
-        fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?fields=title')
+        // fetch('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getList&access_token=d9a34c3816794edfc8b8bd433313f42c')
+        fetch('https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getObjects&access_token=d9a34c3816794edfc8b8bd433313f42c&query=<QUERY> &on_display=1 &per_page=500')
+
+            //curl -X GET 'https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getObjects&access_token=<TOKEN>&query=<QUERY> &accession_number=<ACCESSION_NUMBER> &color=<COLOR> &department_id=<DEPARTMENT_ID> &description=<DESCRIPTION> &on_display=<ON_DISPLAY> &display_date=<DISPLAY_DATE> &exhibition=<EXHIBITION> &exhibition_id=<EXHIBITION_ID> &has_images=<HAS_IMAGES> &has_no_known_copyright=<HAS_NO_KNOWN_COPYRIGHT> &justification=<JUSTIFICATION> &location=<LOCATION> &woe_id=<WOE_ID> &medium=<MEDIUM> &medium_id=<MEDIUM_ID> &period=<PERIOD> &period_id=<PERIOD_ID> &person=<PERSON> &person_id=<PERSON_ID> &role=<ROLE> &role_id=<ROLE_ID> &person_role_id=<PERSON_ROLE_ID> &tag=<TAG> &tag_id=<TAG_ID> &title=<TITLE> &type=<TYPE> &type_id=<TYPE_ID> &year_acquired=<YEAR_ACQUIRED> &year_end=<YEAR_END> &year_start=<YEAR_START> &width=<WIDTH> &height=<HEIGHT> &depth=<DEPTH> &longestside=<LONGESTSIDE> &shortestside=<SHORTESTSIDE>'
+
         .then(res => res.json())
         .then((data) => {
-            this.setState({ met: data })
-            // console.log(data, 'met')
+            this.setState({ cooper: data})
+            console.log(data, 'cooper')
+            //d9a34c3816794edfc8b8bd433313f42c≈
         })
-        fetch('https://collections.louvre.fr/ark:/53355/cl010061995.json', {mode: "no-cors"})
-        .then((data) => {
-            this.setState({ louvre: data })
-            // console.log(data, 'louvreee')
-        })
-        // fetch('https://api.si.edu/openaccess/api/v1.0/content/:id&api_key=Si4jTeolK75j8qglHIg5Zdb3SD4eBlegILEAdYXe')
+
+        // fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?fields=title')
+        // .then(res => res.json())
+        // .then((data) => {
+        //     this.setState({ met: data })
+        //     // console.log(data, 'met')
+        // })
+        // fetch('https://collections.louvre.fr/ark:/53355/cl010061995.json', {mode: "no-cors"})
+        // .then((data) => {
+        //     this.setState({ louvre: data })
+        //     // console.log(data, 'louvreee')
+        // })
+        // // fetch('https://api.si.edu/openaccess/api/v1.0/content/:id&api_key=Si4jTeolK75j8qglHIg5Zdb3SD4eBlegILEAdYXe')
         // //Si4jTeolK75j8qglHIg5Zdb3SD4eBlegILEAdYXe
         // .then(res => res.json())
         // .then((data) => {
